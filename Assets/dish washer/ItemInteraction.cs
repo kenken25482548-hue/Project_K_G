f20fda7ce088f305bf5055460d2221cb7cbe8613
Gbabuuuuuuu <guidemyotaku@gmail.com>
2026-03-27 22:51:06 +0700

    asd

--- Assets/dish washer/ItemInteraction.cs
+++ Assets/dish washer/ItemInteraction.cs
@@ -3,118 +3,76 @@ using TMPro;
 
 public class ItemInteraction : MonoBehaviour
 {
-    [Header("UI")]
     public GameObject interactUI;
     public GameObject infoPanel;
     public TextMeshProUGUI infoText;
 
-    [Header("Item Setting")]
-    [TextArea]
     public string itemInfo = "ข้อมูลไอเทม";
 
-    [Header("Pickup Setting")]
     public Transform holdPoint;
     public Transform dropPoint;
 
-    private bool isNear = false;
+    private bool playerInRange = false;
     private bool isHolding = false;
-
-    // 👉 เก็บตำแหน่งเริ่มต้น
-    private Vector3 startPos;
-    private Quaternion startRot;
 
     void Start()
     {
-        // 🔥 จำตำแหน่งเริ่มต้น
-        startPos = transform.position;
-        startRot = transform.rotation;
-
-        // 🔥 บังคับสถานะเริ่มต้น
-        isHolding = false;
-
-        // 🔥 ป้องกันติด parent ตั้งแต่แรก
-        transform.SetParent(null);
-
-        // 🔥 เปิด collider
-        GetComponent<Collider>().enabled = true;
-
-        // 🔥 ปิด UI
         interactUI.SetActive(false);
         infoPanel.SetActive(false);
-
-        // 🔥 ใส่ข้อความ
-        if (infoText != null)
-            infoText.text = itemInfo;
     }
 
     void Update()
     {
-        if (!isNear) return;
-
-        // กด E = เปิด/ปิดข้อมูล
-        if (Input.GetKeyDown(KeyCode.E))
+        if (playerInRange)
         {
-            infoPanel.SetActive(!infoPanel.activeSelf);
-        }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ToggleInfo();
+            }
 
-        // กด F = หยิบ / วาง
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (!isHolding)
-                PickUp();
-            else
-                Drop();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickupItem();
+            }
         }
     }
 
-    void PickUp()
+    void ToggleInfo()
     {
-        if (holdPoint == null) return;
-
-        isHolding = true;
-
-        transform.SetParent(holdPoint);
-        transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.identity;
-
-        GetComponent<Collider>().enabled = false;
+        infoPanel.SetActive(!infoPanel.activeSelf);
+        infoText.text = itemInfo;
     }
 
-    void Drop()
+    void PickupItem()
     {
-        isHolding = false;
-
-        transform.SetParent(null);
-
-        if (dropPoint != null)
+        if (!isHolding)
         {
-            transform.position = dropPoint.position;
+            transform.position = holdPoint.position;
+            transform.parent = holdPoint;
+            isHolding = true;
         }
         else
         {
-            // 👉 ถ้าไม่มี dropPoint กลับที่เดิม
-            transform.position = startPos;
-            transform.rotation = startRot;
+            transform.position = dropPoint.position;
+            transform.parent = null;
+            isHolding = false;
         }
-
-        GetComponent<Collider>().enabled = true;
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isNear = true;
+            playerInRange = true;
             interactUI.SetActive(true);
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isNear = false;
-
+            playerInRange = false;
             interactUI.SetActive(false);
             infoPanel.SetActive(false);
         }
