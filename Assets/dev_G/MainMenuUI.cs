110f4fc313070efade536efd7a38d8db75cd4195
Gbabuuuuuuu <guidemyotaku@gmail.com>
2026-06-28 18:03:58 +0700

    กหฟ

---
+++ Assets/dev_G/MainMenuUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("ใส่ชื่อ Scene ด่านแรก")]
+    public string firstSceneName = "1bathroom1";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstSceneName);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
